dc692aff6b00920a13fbcb53224dfe7887dcc8be
riskimeji <meji.riski123@gmail.com>
2024-05-02 08:18:40 +0700

    fox for karma

--- app.js
+++ app.js
@@ -3,36 +3,28 @@ const fs = require('fs');
 const readline = require('readline');
 
 const url =
-  'https://docs.google.com/forms/d/e/1FAIpQLSf91Cevtd_heNf7IItyGYxy_rM65Ulm6yjn43R6GQkH1ku-yA/formResponse'; // URL form response
+  'https://docs.google.com/forms/d/e/1FAIpQLSf07dmJH1MbAroENm2pQPVoN99-FcGEhnToMj3OBgs2R4B_NQ/viewform';
 
 const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   Origin: 'https://docs.google.com',
-  Referer: url,
-  // 'User-Agent': 'Your User Agent', // Jika perlu sesuaikan dengan user agent Anda
+  Referer:
+    'https://docs.google.com/forms/d/e/1FAIpQLSf07dmJH1MbAroENm2pQPVoN99-FcGEhnToMj3OBgs2R4B_NQ/viewform',
 };
 
 const rl = readline.createInterface({
-  input: fs.createReadStream('data.txt'), // Baca file data.txt
+  input: fs.createReadStream('data.txt'),
   crlfDelay: Infinity,
 });
 
-rl.on('line', (line) => {
-  const data = {};
-  const values = line.split('|'); // Pisahkan data berdasarkan '|'
-  data['entry.2005620554'] = values[0]; // Nama Anda
-  data['entry.1045781291'] = values[1]; // Email
-  data['entry.1065046570'] = values[2]; // Alamat
-  data['entry.1166974658'] = values[3]; // Nomor Telepon
-  data['entry.839337160'] = values[4]; // Komentar
+rl.on('line', async (line) => {
+  const data = new URLSearchParams();
+  data.append('entry.1908151957', line.trim());
 
-  // Kirim permintaan POST ke formulir dengan header
-  axios
-    .post(url, data, { headers })
-    .then((response) => {
-      console.log('Data berhasil dikirim ke formulir Google.');
-    })
-    .catch((error) => {
-      console.error('Gagal mengirim data ke formulir Google:', error);
-    });
+  try {
+    await axios.post(url, data, { headers });
+    console.log('Data berhasil dikirim ke formulir Google:', line.trim());
+  } catch (error) {
+    console.error('Gagal mengirim data ke formulir Google:', error);
+  }
 });
